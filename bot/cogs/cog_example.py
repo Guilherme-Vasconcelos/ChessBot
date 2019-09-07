5d4633ad89adc24c215a2e2a28ee082e76de99f3
Guilherme-Vasconcelos <49197151+Guilherme-Vasconcelos@users.noreply.github.com>
2019-09-07 01:46:26 -0300

    Added something

--- bot/cogs/cog_example.py
+++ bot/cogs/cog_example.py
@@ -2,10 +2,12 @@ import re
 
 from discord.ext import commands
 
+from bot.bot_client import Bot
+
 
 class CogExample(commands.Cog):
 
-    def __init__(self, bot):
+    def __init__(self, bot: Bot):
         self.bot = bot
 
     @commands.has_permissions(manage_channels=True)
