dac908384cf9603381a15208de2a036a72665343
Guilherme-Vasconcelos <49197151+Guilherme-Vasconcelos@users.noreply.github.com>
2019-09-09 12:41:18 -0300

    Fixed ASCII art

--- bot/cogs/chess.py
+++ bot/cogs/chess.py
@@ -5,15 +5,17 @@ from bot.bot_client import Bot
 
 board = chess.Board()
 
+
 class Chess(commands.Cog):
 
     def __init__(self, bot: Bot):
         self.bot = bot
 
     @commands.has_permissions(manage_channels=True)
-    @commands.command(aliases=['example'])
+    @commands.command(aliases=[''])
     async def challenge(self, ctx: commands.Context, challenged: discord.Member):
-        await ctx.send(f'{challenged.mention}, you\'ve been challenged to a chess game! Here\'s the board:\n{board}')
+        await ctx.send(f'{challenged.mention}, you\'ve been challenged to a chess game by {ctx.message.author.mention}! Here\'s the board:\n```{board}```')
+
 
 def setup(bot):
     bot.add_cog(Chess(bot))
