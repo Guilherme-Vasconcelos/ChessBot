355e296e39b98fe8ba7e485b4192cd1bddc45eb8
Guilherme-Vasconcelos <49197151+Guilherme-Vasconcelos@users.noreply.github.com>
2019-09-09 13:33:55 -0300

    Fixed linter things, added a few comments

--- bot/cogs/chess.py
+++ bot/cogs/chess.py
@@ -14,7 +14,14 @@ class Chess(commands.Cog):
     @commands.has_permissions(manage_channels=True)
     @commands.command(aliases=[''])
     async def challenge(self, ctx: commands.Context, challenged: discord.Member):
-        await ctx.send(f'{challenged.mention}, you\'ve been challenged to a chess game by {ctx.message.author.mention}! Here\'s the board:\n```{board}```')
+        """
+        This command !challenge is going to be used in order to challenge a player to a game of chess
+        it takes one argument, which is the player, so it should be used like this: !challenge @Player
+        """
+        await ctx.send(
+            f'{challenged.mention}, you\'ve been challenged to a chess game by'
+            f'{ctx.message.author.mention}! Here\'s the board:\n```{board}```'
+        )
 
 
 def setup(bot):
