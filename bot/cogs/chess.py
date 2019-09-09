fe0841faf70527539d22ece262eac4b0ca2a4f33
Guilherme-Vasconcelos <49197151+Guilherme-Vasconcelos@users.noreply.github.com>
2019-09-09 15:32:39 -0300

    Improved the way the game works

--- bot/cogs/chess.py
+++ bot/cogs/chess.py
@@ -21,18 +21,25 @@ class Chess(commands.Cog):
 
         await ctx.send(
             f'{challenged.mention}, you\'ve been challenged to a chess game by'
-            f'{ctx.message.author.mention}! Here\'s the board:\n```{board}```'
+            f'{ctx.message.author.mention}! Here\'s the board:\n'
         )
+
+        board_message = await ctx.send(f'```{board}```')
 
         while True:
             #  Game begins by asking the challenger's move, updating board and then showing the updated board
-            msg = await self.bot.wait_for('message', check=lambda message: message.author == ctx.author)
-            board.push_san(msg.content)
-            await ctx.send(f'```{board}```')
+            msg = await self.bot.wait_for('message', check=lambda message: message.author == ctx.author)  # gets move
+            board.push_san(msg.content)  # updates board
+            #  the line below edits bot's message in order to show the updated board and shows his move
+            await board_message.edit(content=f'```{board}```\n{ctx.message.author.mention} played {msg.content}')
+            await msg.delete()  # deletes player's message
+
             #  Asks for the challenged's move, updates board and then shows the updated board
-            msg2 = await self.bot.wait_for('message', check=lambda message: message.author == challenged)
-            board.push_san(msg2.content)
-            await ctx.send(f'```{board}```')
+            msg2 = await self.bot.wait_for('message', check=lambda message: message.author == challenged)  # gets move
+            board.push_san(msg2.content)  # updates board
+            #  the line below edits bot's message in order to show the updated board and shows his move
+            await board_message.edit(content=f'```{board}```\n{challenged.mention} played {msg2.content}')
+            await msg2.delete()  # deletes player's message
 
 
 def setup(bot):
