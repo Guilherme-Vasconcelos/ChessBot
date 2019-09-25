2f5b5d60b8312c01ed81b764243adc8bf77bfec2
Guilherme-Vasconcelos <49197151+Guilherme-Vasconcelos@users.noreply.github.com>
2019-09-25 06:13:54 -0300

    Added a few comments to help.py file

--- bot/cogs/help.py
+++ bot/cogs/help.py
@@ -10,14 +10,18 @@ class Help(commands.Cog):
 
     @commands.has_permissions(manage_channels=True)
     @commands.command(aliases=['chess', 'chessbot'])
-    async def help(self, ctx: commands.Context):
-        embed_help = discord.Embed(title='Hello! Here are all my commands:', color=0x0473b3)
-        embed_help.add_field(name='!help', value='Lists all available commands')
-        embed_help.add_field(
+    async def help(self, ctx: commands.Context):  # creates help command
+        """
+        This function creates a rich embed with all the commands available
+        for the bot, i.e. !help and !challenge
+        """
+        embed_help = discord.Embed(title='Hello! Here are all my commands:', color=0x0473b3)  # creates embed
+        embed_help.add_field(name='!help', value='Lists all available commands')  # help command
+        embed_help.add_field(  # challenge command
             name='!challenge [player]',
             value='Starts a game against [player]. Remember to mention them!'
                              )
-        await ctx.send(embed=embed_help)
+        await ctx.send(embed=embed_help)  # sends embed
 
 
 def setup(bot):
