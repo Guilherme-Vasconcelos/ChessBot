972ce8c504c33a54682e822d0588b63d3aa2b0e2
Guilherme-Vasconcelos <49197151+Guilherme-Vasconcelos@users.noreply.github.com>
2019-09-30 09:58:38 -0300

    [Closes #15] Player now can select his color

--- bot/cogs/chess.py
+++ bot/cogs/chess.py
@@ -27,34 +27,34 @@ class Chess(commands.Cog):
         return f'The game is over! The winner is {winner.mention}.'
 
     @commands.has_permissions(manage_channels=True)
-    @commands.command(aliases=['chalenge', 'challeng', 'fight', 'play', 'chaleng'])
-    async def challenge(self, ctx: commands.Context, challenged: discord.Member):
+    @commands.command(aliases=['chalenge', 'challeng', 'fight', 'challenge', 'chaleng'])
+    async def play(self, ctx: commands.Context, white_player: discord.Member, black_player: discord.Member):
         """
-        This command !challenge is going to be used in order to challenge a player to a game of chess
-        it takes one argument, which is the player, so it should be used like this: !challenge @Player
+        This command !play is going to be used in order to start a game of chess between both players.
+        First name is white, second name is black. It should be used like this: !play @Player1 @Player2
         """
-        def check_challenger_move(message: discord.Message) -> bool:
+        def check_white_move(message: discord.Message) -> bool:
             """
             This function will verify if the first, third, etc. messages during game are valid
-            i.e. it must either be a MOVE sent by the PLAYER 1, or a RESIGN message sent by any player
+            i.e. it must either be a MOVE sent by the white_player, or a RESIGN message sent by any player
             or a DRAW message sent by any player
             """
-            return (message.author == ctx.author or message.content.lower() in ('resign', 'draw'))
+            return (message.author == white_player or message.content.lower() in ('resign', 'draw'))
 
-        def check_challenged_move(message: discord.Message) -> bool:
+        def check_black_move(message: discord.Message) -> bool:
             """
             This function will verify if the second, fourth, etc. messages during game are valid
-            i.e. it must either be a MOVE sent by the PLAYER 2, or a RESIGN message sent by any player
+            i.e. it must either be a MOVE sent by the black_player, or a RESIGN message sent by any player
             or a DRAW message sent by any player
             """
-            return (message.author == challenged or message.content.lower() in ('resign', 'draw'))
+            return (message.author == black_player or message.content.lower() in ('resign', 'draw'))
 
         board = Board()  # creates an instance of Board class, which is going to be used during the game
 
-        # sends a message to let the challenged know who challenged them
+        # sends a message to let the players know about their pieces' color
         await ctx.send(
-            f'{challenged.mention}, you\'ve been challenged to a chess game by '
-            f'{ctx.message.author.mention}!\n'
+            f'{white_player.mention} will play with white pieces and '
+            f'{black_player.mention} plays with black pieces!\n'
             f'Check out the board below:\n'
         )
 
@@ -67,30 +67,30 @@ class Chess(commands.Cog):
         )
 
         board_message = await ctx.send(embed=embed)  # sends the board (also saves it on board_message)
-        player2_invalid_move = False  # checks if player 2 has made an invalid move
-        player2_refused_draw = False  # checks if player 2 has refused a draw
+        black_invalid_move = False  # checks if black_player has made an invalid move
+        black_refused_draw = False  # checks if black_player has refused a draw
 
         while True:
-            if not (player2_invalid_move or player2_refused_draw):
+            if not (black_invalid_move or black_refused_draw):
                 #  if player 2 makes an invalid move, this block will not execute, thus making player 2 repeat his move
-                #  game begins by asking the challenger's move, updating board and then showing the updated board
+                #  game begins by asking the white's move, updating board and then showing the updated board
                 #  the line below will get the player's move
-                challenger_msg = await self.bot.wait_for('message', check=check_challenger_move)
+                white_msg = await self.bot.wait_for('message', check=check_white_move)
 
-                if challenger_msg.content.lower() == 'resign':
+                if white_msg.content.lower() == 'resign':
                     # if the message is 'resign' (sent by any player), the game will end
-                    return await ctx.send(f'{challenger_msg.author.mention} resigns! The game is over!')
-                if challenger_msg.content.lower() == 'draw':
+                    return await ctx.send(f'{white_msg.author.mention} resigns! The game is over!')
+                if white_msg.content.lower() == 'draw':
                     # if the message is 'draw' (sent by any player), the bot must wait for the other player's response
-                    await board_message.edit(content=f'{challenger_msg.author.mention} offers a draw! Type `draw`'
+                    await board_message.edit(content=f'{white_msg.author.mention} offers a draw! Type `draw`'
                                              f' in order to accept it or anything else to decline it!', embed=embed)
-                    bot_message = await ctx.send(f'{challenger_msg.author.mention} offers a draw! Type `draw` '
+                    bot_message = await ctx.send(f'{white_msg.author.mention} offers a draw! Type `draw` '
                                                  f'in order to accept it or anything else to decline it!')
 
                     # if message's author is equal to whoever sent the challenge
-                    if challenger_msg.author == ctx.author:
+                    if white_msg.author == white_player:
                         # waits for other player's response
-                        response = await self.bot.wait_for('message', check=lambda m: m.author == challenged)
+                        response = await self.bot.wait_for('message', check=lambda m: m.author == black_player)
                         if response.content.lower() == 'draw':  # if the response is draw then the game draws
                             await board_message.edit(content=f'The game is a draw!', embed=embed)
                             return await ctx.send('The game is a draw!')
@@ -99,11 +99,11 @@ class Chess(commands.Cog):
                             await board_message.edit(content=f'Draw declined!', embed=embed)
                             await bot_message.delete()
                             await response.delete()
-                            await challenger_msg.delete()
+                            await white_msg.delete()
                             continue
-                    elif challenger_msg.author == challenged:  # if message's author is equal to the challenged person
+                    elif white_msg.author == black_player:  # if message's author is equal to the challenged person
                         # waits for other player's response
-                        response = await self.bot.wait_for('message', check=lambda m: m.author == ctx.author)
+                        response = await self.bot.wait_for('message', check=lambda m: m.author == white_player)
                         if response.content.lower() == 'draw':  # if the response is draw then the game draws
                             await board_message.edit(content=f'The game is a draw!', embed=embed)
                             return await ctx.send('The game is a draw!')
@@ -112,22 +112,22 @@ class Chess(commands.Cog):
                             await board_message.edit(content=f'Draw declined!', embed=embed)
                             await bot_message.delete()
                             await response.delete()
-                            await challenger_msg.delete()
+                            await white_msg.delete()
                             continue
                 try:
-                    board.push_san(challenger_msg.content)  # updates board
+                    board.push_san(white_msg.content)  # updates board
                 except ValueError:
                     # player 1 has made an illegal move
                     embed.set_image(url=f'http://www.fen-to-image.com/image/{board.fen().split()[0]}')  # updates image
                     await board_message.edit(content=self.illegal_msg(ctx.author), embed=embed)
-                    await challenger_msg.delete()
+                    await white_msg.delete()
                     continue
 
                 #  the line below edits bot's message in order to show the updated board and shows his move
                 embed.set_image(url=f'http://www.fen-to-image.com/image/{board.fen().split()[0]}')  # updates image
-                embed.set_footer(text=f'Last move: {challenger_msg.content} by white')
+                embed.set_footer(text=f'Last move: {white_msg.content} by white')
                 await board_message.edit(content='', embed=embed)
-                await challenger_msg.delete()  # deletes player's message
+                await white_msg.delete()  # deletes player's message
                 if board.is_drawn():  # checks if the current position is a draw
                     return await ctx.send('The game is a draw!')
                 elif board.is_checkmate():  # checks if the current position is checkmate
@@ -135,69 +135,69 @@ class Chess(commands.Cog):
 
             #  asks for the challenged's move, updates board and then shows the updated board
             #  the line below will get the player's move
-            challenged_msg = await self.bot.wait_for('message', check=check_challenged_move)
+            black_msg = await self.bot.wait_for('message', check=check_black_move)
 
-            if challenged_msg.content.lower() == 'resign':
+            if black_msg.content.lower() == 'resign':
                 # if the message is 'resign' (sent by any player), the game will end
-                return await ctx.send(f'{challenged_msg.author.mention} resigns! The game is over!')
+                return await ctx.send(f'{black_msg.author.mention} resigns! The game is over!')
 
-            if challenged_msg.content.lower() == 'draw':
+            if black_msg.content.lower() == 'draw':
                 # if the message is 'draw' (sent by any player), the bot must wait for the other player's response
                 draw_msg = (
-                    f'{challenged_msg.author.mention} offers a draw! Type `draw`'
+                    f'{black_msg.author.mention} offers a draw! Type `draw`'
                     f' in order to accept it or anything else to decline it!'
                 )
                 await board_message.edit(content=draw_msg, embed=embed)
                 bot_message = await ctx.send(draw_msg)
 
-                if challenged_msg.author == ctx.author:  # if message's author is equal to whoever sent the challenge
+                if black_msg.author == white_player:  # if message's author is equal to whoever sent the challenge
                     # gets response from the other player
-                    response = await self.bot.wait_for('message', check=lambda msg: msg.author == challenged)
+                    response = await self.bot.wait_for('message', check=lambda msg: msg.author == black_player)
 
                     if response.content.lower() == 'draw':  # if response is draw then the game draws
                         await board_message.edit(content=f'The game is a draw!', embed=embed)
                         return await ctx.send('The game is a draw!')
                     else:  # if response is not draw then the game continues
                         await board_message.edit(content=f'Draw declined!', embed=embed)
-                        await challenged_msg.delete()
+                        await black_msg.delete()
                         await response.delete()
                         await bot_message.delete()
-                        player2_refused_draw = True
+                        black_refused_draw = True
                         continue
-                elif challenged_msg.author == challenged:  # if message's author is equal to challenged player
+                elif black_msg.author == black_player:  # if message's author is equal to challenged player
                     # gets response from the other player
-                    response = await self.bot.wait_for('message', check=lambda m: m.author == ctx.author)
+                    response = await self.bot.wait_for('message', check=lambda m: m.author == white_player)
                     if response.content.lower() == 'draw':  # if response is draw then the game draws
                         await board_message.edit(content=f'The game is a draw!', embed=embed)
                         return await ctx.send('The game is a draw!')
                     else:  # if response is not draw then the game continues
                         await board_message.edit(content=f'Draw declined!', embed=embed)
-                        await challenged_msg.delete()
+                        await black_msg.delete()
                         await response.delete()
                         await bot_message.delete()
-                        player2_refused_draw = True
+                        black_refused_draw = True
                         continue
             try:
-                player2_invalid_move = False
-                player2_refused_draw = False
-                board.push_san(challenged_msg.content)  # updates board
+                black_invalid_move = False
+                black_refused_draw = False
+                board.push_san(black_msg.content)  # updates board
             except ValueError:
                 # player 2 has made an illegal move
                 embed.set_image(url=f'http://www.fen-to-image.com/image/{board.fen().split()[0]}')  # updates image
-                await board_message.edit(content=self.illegal_msg(challenged), embed=embed)
-                await challenged_msg.delete()
-                player2_invalid_move = True
+                await board_message.edit(content=self.illegal_msg(black_player), embed=embed)
+                await black_msg.delete()
+                black_invalid_move = True
                 continue
 
             #  the line below edits bot's message in order to show the updated board and shows his move
             embed.set_image(url=f'http://www.fen-to-image.com/image/{board.fen().split()[0]}')  # updates image
-            embed.set_footer(text=f'Last move: {challenged_msg.content} by black')
+            embed.set_footer(text=f'Last move: {black_msg.content} by black')
             await board_message.edit(content='', embed=embed)
-            await challenged_msg.delete()
+            await black_msg.delete()
             if board.is_drawn():
                 return await ctx.send('The game is a draw!')
             elif board.is_checkmate():
-                return await ctx.send(self.game_over_msg(winner=challenged))
+                return await ctx.send(self.game_over_msg(winner=black_player))
 
 
 def setup(bot):
--- bot/cogs/help.py
+++ bot/cogs/help.py
@@ -13,13 +13,15 @@ class Help(commands.Cog):
     async def help(self, ctx: commands.Context):  # creates help command
         """
         This function creates a rich embed with all the commands available
-        for the bot, i.e. !help and !challenge
+        for the bot, i.e. !help and !play
         """
         embed_help = discord.Embed(title='Hello! Here are all my commands:', color=0x0473b3)  # creates embed
         embed_help.add_field(name='!help', value='Lists all available commands')  # help command
-        embed_help.add_field(  # challenge command
-            name='!challenge [player]',
-            value='Starts a game against [player]. Remember to mention them!'
+        embed_help.add_field(  # play command
+            name='!play [white_player] [black_player]',
+            value='Starts a game against between [white_player] '
+                  'and [black_player].\nRemember to mention them and not to include the square brackets!'
+                  '\nUsage example: !play @Paul @Lily'
                              )
         await ctx.send(embed=embed_help)  # sends embed
 
